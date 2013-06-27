792c4b03e2be25838fcbb8589d4202c20634c853
tkhsh <uki.thashi@gmail.com>
2013-06-27 12:09:04 +0900

    ひっくり返す機能を追加（途中。バグあり）

--- app.js
+++ app.js
@@ -43,8 +43,10 @@ $(function() {
 
 		//白の番か黒の番かを判定
 		if(turn) {
-			board[position] = "●";
-			turn = false;
+			if(flipDetection(position)) {
+				board[position] = "●";
+				turn = false;
+			}
 		} else {
 			board[position] = "○";
 			turn = true;
@@ -52,6 +54,74 @@ $(function() {
 
 		draw();
 	});
+
+	/*
+	function parsePosition (boardNum) {
+		var tmpY = boardNum / 4;
+		var tmpX = boardNum % 4;
+		return {
+			posX:tmpX,
+			posY:tmpY
+		}
+	}
+	*/
+
+	//石がひっくり返るか判定
+	function flipDetection(stonePos) {
+		
+		var canSnap = false; // 石がおけるか？
+
+		for(var dx = -1; dx <= 1; dx++) {
+			for(var dy = -1; dy <= 1; dy++) {
+
+				var posX = stonePos%4;
+				var posY = parseInt(stonePos/4);
+				var isContinueSearch = true; // TODO:終了条件の追加（1.端まで来たとき 2.検索結果が●だったとき）
+				var differentStones = [];
+
+				while(isContinueSearch) {
+					posX += dx;
+					posY += dy;
+					console.log(" posX:" + posX);
+					console.log(" posY:" + posY);
+
+					if(posX < 0 || posX > 4) { //端まできたときにsearchを終了
+						break;
+					} else if (posY < 0 || posY > 4) {
+						break;
+					}
+
+					var boardNum = posY*4 + posX%4;
+					console.log(" boardNum:" + boardNum);
+
+					if(board[boardNum] == "○") { // 検索結果が異なる色（白と仮定）なら
+						// このブロック内の処理（1.boardNum（座標）を記録 2.石が置ける　3.同じ方向にもう一マス進み、検索する。）
+
+						differentStones.push(boardNum); // 1を実装。differentStonesが空でなければ石は置けるとするので２の条件も満たす
+						//whileループが継続するので、「3.同じ方向にもう一マス進み、検索する」の条件を満たす。
+
+					} else if(board[boardNum] == "●") { // 検索結果が同じ色（黒と仮定）なら
+						// このブロック内の処理（1.この方向の検索が終了 2.石が置けるなら、記録した座標の色を変える）
+						isContinueSearch = false; // 1を実装
+						if(differentStones.length != 0) { //2を実装
+							/////////////////////////関数に切り出したい処理///////////////////////////////
+							for(var i = 0; i < differentStones.length; i++) {
+								var tmp = differentStones[i];
+								board[tmp] = "●"; // 黒だと仮定
+							}
+							/////////////////////////////////////////////////////////
+							canSnap = true;
+						} else { //消す予定のブロック
+							 //石が置けない
+						}
+					} else { // 間にスペースのある場合　消す予定のブロック
+						// 石が置けない
+					}
+				}
+			}
+		}
+		return canSnap;
+	}
 
 	draw();
 });
