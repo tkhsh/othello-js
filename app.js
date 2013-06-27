ce31853a620d0900269c934dcba8742d8ab5111d
tkhsh <uki.thashi@gmail.com>
2013-06-27 14:44:35 +0900

    マジックナンバーをグローバル変数として定義

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(function() {
+	//オセロ盤のサイズを定義
+	var boardSize = 4;
 	// オセロ盤のglobal変数
-	var board = new Array(16);
+	var board = new Array(boardSize*boardSize);
 	// オセロ盤を初期化
 	for(var i = 0; i < board.length; i++) {
 		board[i] = "□";
@@ -15,7 +17,7 @@ $(function() {
 		// 配列boardをStringに変換
 		var boardStr = "";
 		for(var i = 0; i < board.length; i++) { 
-			if(i%4 == 0) {
+			if(i%boardSize == 0) {
 				boardStr += "\n"
 			}
 			boardStr += board[i];
@@ -74,8 +76,8 @@ $(function() {
 		for(var dx = -1; dx <= 1; dx++) {
 			for(var dy = -1; dy <= 1; dy++) {
 
-				var posX = stonePos%4;
-				var posY = Math.floor(stonePos/4); // 整数の値をとる
+				var posX = stonePos%boardSize;
+				var posY = Math.floor(stonePos/boardSize); // 整数の値をとる
 				var isContinueSearch = true; // TODO:終了条件の追加（1.端まで来たとき 2.検索結果が●だったとき）
 				var differentStones = [];
 
@@ -85,13 +87,13 @@ $(function() {
 					console.log(dx + " dy:" + dy + " posX:" + posX);
 					console.log(" posY:" + posY);
 
-					if(posX < 0 || posX > 4) { //端まできたときにsearchを終了
+					if(posX < 0 || posX > boardSize) { //端まできたときにsearchを終了
 						break;
-					} else if (posY < 0 || posY > 4) {
+					} else if (posY < 0 || posY > boardSize) {
 						break;
 					}
 
-					var boardNum = posY*4 + posX%4;
+					var boardNum = posY*boardSize + posX%boardSize;
 					console.log(" boardNum:" + boardNum);
 
 					if(board[boardNum] == oppositeStone) { // 検索結果が異なる色（白と仮定）なら
